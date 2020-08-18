483f4284ef9afaf9393ed470f21a405533ded1ed
sravan <shashilsravan.ss.ss@gmail.com>
2020-08-18 09:59:14 +0530

    Updates

---
+++ precache-manifest.2d12c2476ad09591c322c428390cffb2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74915df01694ad2f6ff32cd75759139",
+    "url": "/resort-Web/index.html"
+  },
+  {
+    "revision": "798945959fdfb9574feb",
+    "url": "/resort-Web/static/css/main.b04c930f.chunk.css"
+  },
+  {
+    "revision": "942a6b9bf6eefe150af4",
+    "url": "/resort-Web/static/js/2.645ff7ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resort-Web/static/js/2.645ff7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798945959fdfb9574feb",
+    "url": "/resort-Web/static/js/main.082aecda.chunk.js"
+  },
+  {
+    "revision": "2fa0ba012917ec1b707d",
+    "url": "/resort-Web/static/js/runtime-main.153e8c71.js"
+  },
+  {
+    "revision": "43d319216be7013e3315e650e9c2da23",
+    "url": "/resort-Web/static/media/832.43d31921.gif"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/resort-Web/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/resort-Web/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/resort-Web/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/resort-Web/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e50a9b6e2df735d966fa2932801953ed",
+    "url": "/resort-Web/static/media/logo.e50a9b6e.png"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/resort-Web/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/resort-Web/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/resort-Web/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/resort-Web/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/resort-Web/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/resort-Web/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/resort-Web/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/resort-Web/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/resort-Web/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/resort-Web/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/resort-Web/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/resort-Web/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
